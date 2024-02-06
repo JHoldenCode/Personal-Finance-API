23023ecdd097f27aae704dc52cb7f69f96fea7a1
Jack Holden <John_Holden@student.uml.edu>
2024-02-05 22:26:28 -0500

    finish range of purchases endpoint

--- endpoints/money_spent/app.py
+++ endpoints/money_spent/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, jsonify
 import json
-import request
+from datetime import datetime
 
 # example curl request
 # curl -X POST http://127.0.0.1:5000/money_spent -H "Content-Type: application/json" -d @input_post_purchases.json
 app = Flask(__name__)
 DB_FILE_PATH = '../../databases/money_spent/purchases.json'
+
+# HELPER METHODS
+
+def transfer_purchases_from_json(return_purchases, current_purchases, date):
+    month, day, year = date.split('/')
+    if year not in return_purchases:
+        return_purchases[year] = {}
+
+    if month not in return_purchases[year]:
+        return_purchases[year][month] = {}
+
+    return_purchases[year][month][date] = current_purchases[year][month][date]
+
+
+# ENDPOINTS
 
 @app.route('/money_spent', methods=['POST'])
 def post_money_spent():
@@ -65,7 +80,7 @@ def get_range_of_purchases():
 
     # error handling with date range args
     if not start_date and not end_date:
-        return "Must submit a start and/or end date for this endpoint through the args 'start' and 'end'.", 409
+        return "Must submit a start and/or end date for this endpoint using the args 'start' and 'end'.\nUse the /money_spent/all_purchases endpoint to get all purchase records.", 409
     if not start_date:
         # set arbitrary start date from long ago to include purchases up until end_date
         start_date = '9/15/1215'
@@ -73,7 +88,31 @@ def get_range_of_purchases():
         # set arbitrary end date far in future to include purchases after start date
         end_date = '1/24/5090'
 
-    
+    # check that end date is after or on same day as start date
+    start_date_timestamp = datetime.strptime(start_date, '%m/%d/%Y')
+    end_date_timestamp = datetime.strptime(end_date, '%m/%d/%Y')
+    if start_date_timestamp > end_date_timestamp:
+        return 'Unable to retrieve purchases in a range where start date is after end date.', 410
+
+    start_month, start_day, start_year = start_date.split('/')
+    end_month, end_day, end_year = end_date.split('/')
+
+    # append purchase data within range of dates to return_json
+    return_json = {'purchases': {}}
+    with open(DB_FILE_PATH, 'r') as db:
+        # get view of current purchases
+        current_purchases_json = json.load(db)
+        current_purchases = current_purchases_json['purchases']
+
+        for year in current_purchases:
+            if year >= start_year and year <= end_year:
+                for month in current_purchases[year]:
+                    for date in current_purchases[year][month]:
+                        date_timestamp = datetime.strptime(date, '%m/%d/%Y')
+                        if date_timestamp >= start_date_timestamp and date_timestamp <= end_date_timestamp:
+                            transfer_purchases_from_json(return_json['purchases'], current_purchases, date)
+
+    return jsonify(return_json), 200
 
 
 
