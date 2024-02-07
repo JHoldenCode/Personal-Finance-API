a47d92a390a29549b4f641291a67c385e5d1d4d6
Jack Holden <John_Holden@student.uml.edu>
2024-02-07 17:06:07 -0500

    added summation category endpoint and some refactoring

--- endpoints/money_spent/app.py
+++ endpoints/money_spent/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
 import json
+from flask import Flask, request, jsonify
 from datetime import datetime
+from collections import defaultdict
 
 # example curl request
 # curl -X POST http://127.0.0.1:5000/money_spent -H "Content-Type: application/json" -d @input_post_purchases.json
@@ -18,6 +19,31 @@ def transfer_purchases_from_json(return_purchases, current_purchases, date):
         return_purchases[year][month] = {}
 
     return_purchases[year][month][date] = current_purchases[year][month][date]
+
+def set_start_end_date_defaults(start_date, end_date):
+    if not start_date:
+        # set arbitrary start date from long ago to include purchases up until end_date
+        start_date = '9/15/1215'
+    if not end_date:
+        # set arbitrary end date far in future to include purchases after start date
+        end_date = '1/24/5090'
+
+    return start_date, end_date
+
+def verify_chronological_order(start_date, end_date):
+    # check that end date is after or on same day as start date
+    start_date_timestamp = datetime.strptime(start_date, '%m/%d/%Y')
+    end_date_timestamp = datetime.strptime(end_date, '%m/%d/%Y')
+    if start_date_timestamp > end_date_timestamp:
+        raise ValueError
+    
+    return start_date_timestamp, end_date_timestamp
+
+def sum_purchases_on_date(summation_dict, purchases_on_date):
+    for purchase_obj in purchases_on_date:
+        category = purchase_obj['category']
+        amount = purchase_obj['amount']
+        summation_dict[category] += amount
 
 
 # ENDPOINTS
@@ -74,25 +100,23 @@ def get_all_purchases():
 
 @app.route('/money_spent/range_of_purchases', methods=['GET'])
 def get_range_of_purchases():
+    NO_ARG_MSG = "Must submit a start and/or end date for this endpoint using the args 'start' and 'end'.\nUse the /money_spent/all_purchases endpoint to get all purchase records."
+    NO_ARG_ERR_CODE = 401
+    NOT_CHRONO_MSG = 'Unable to retrieve purchases in a range where start date is after end date.'
+    NOT_CHRONO_ERR_CODE = 402
+
     # get arguments for start and end dates inclusive
     start_date = request.args.get('start')
     end_date = request.args.get('end')
 
-    # error handling with date range args
     if not start_date and not end_date:
-        return "Must submit a start and/or end date for this endpoint using the args 'start' and 'end'.\nUse the /money_spent/all_purchases endpoint to get all purchase records.", 409
-    if not start_date:
-        # set arbitrary start date from long ago to include purchases up until end_date
-        start_date = '9/15/1215'
-    elif not end_date:
-        # set arbitrary end date far in future to include purchases after start date
-        end_date = '1/24/5090'
+        return NO_ARG_MSG, NO_ARG_ERR_CODE
+    start_date, end_date = set_start_end_date_defaults(start_date, end_date)
 
-    # check that end date is after or on same day as start date
-    start_date_timestamp = datetime.strptime(start_date, '%m/%d/%Y')
-    end_date_timestamp = datetime.strptime(end_date, '%m/%d/%Y')
-    if start_date_timestamp > end_date_timestamp:
-        return 'Unable to retrieve purchases in a range where start date is after end date.', 410
+    try:
+        start_date_timestamp, end_date_timestamp = verify_chronological_order(start_date, end_date)
+    except ValueError:
+        return NOT_CHRONO_MSG, NOT_CHRONO_ERR_CODE
 
     start_month, start_day, start_year = start_date.split('/')
     end_month, end_day, end_year = end_date.split('/')
@@ -113,6 +137,50 @@ def get_range_of_purchases():
                             transfer_purchases_from_json(return_json['purchases'], current_purchases, date)
 
     return jsonify(return_json), 200
+
+@app.route('/money_spent/category_summation', methods=['GET'])
+def get_category_summation():
+    NOT_CHRONO_MSG = 'Unable to retrieve purchases in a range where start date is after end date.'
+    NOT_CHRONO_ERR_CODE = 402
+
+    # get arguments for start and end dates inclusive
+    start_date = request.args.get('start')
+    end_date = request.args.get('end')
+
+    start_date, end_date = set_start_end_date_defaults(start_date, end_date)
+
+    try:
+        start_date_timestamp, end_date_timestamp = verify_chronological_order(start_date, end_date)
+    except ValueError:
+        return NOT_CHRONO_MSG, NOT_CHRONO_ERR_CODE
+
+    start_month, start_day, start_year = start_date.split('/')
+    end_month, end_day, end_year = end_date.split('/')
+
+    category_summation_dict = {}
+    category_summation_dict['categories'] = defaultdict(float)
+    current_purchases = {}
+    with open(DB_FILE_PATH, 'r') as db:
+        # get view of current purchases
+        current_purchases_json = json.load(db)
+        current_purchases = current_purchases_json['purchases']
+
+    # TODO - modularize this code with similar code in get_range_of_purchases()
+    for year in current_purchases:
+        if year >= start_year and year <= end_year:
+            for month in current_purchases[year]:
+                for date in current_purchases[year][month]:
+                    date_timestamp = datetime.strptime(date, '%m/%d/%Y')
+                    if date_timestamp >= start_date_timestamp and date_timestamp <= end_date_timestamp:
+                        purchases_on_date = current_purchases[year][month][date]
+                        sum_purchases_on_date(category_summation_dict['categories'], purchases_on_date)
+
+    category_summation_dict['date_range'] = {
+        'start_date': start_date,
+        'end_date': end_date
+    }
+
+    return jsonify(category_summation_dict), 200
 
 
 
